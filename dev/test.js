1605dd2d220c08156fb70ca41ffc4797edff894d
Sebastien Dewaelle <sdewaelle@gmail.com>
2018-08-27 16:19:40 +0200

    Added Blockchain methods

--- dev/test.js
+++ dev/test.js
@@ -2,6 +2,33 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
+console.log(bitcoin);
+/*
+const previousBlockHash = 'sfgFGDSFsd4155CWedggs';
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: 's1ergrehgdgh',
+        recipient: 'r1hgfdhjgdghj'
+    },
+    {
+        amount: 20,
+        sender: 's2gsfdhdfhtr',
+        recipient: 'r2fdjhgddfg'
+    },
+    {
+        amount: 30,
+        sender: 's3gdjhjgfdgc',
+        recipient: 'r3sdfgshgjhk'
+    }
+];
+
+console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+*/
+/*
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+*/
+/*
 
 bitcoin.createNewBlock(238419, 'Mfd57dRFm9A', 'Ffgds474sqDE');
 bitcoin.createNewTransaction(100, 'SEBgDSFS145dDF', 'CELf154sdfgSDFGg');
@@ -14,3 +41,4 @@ bitcoin.createNewTransaction(2000, 'SEBgDSFS145dDF', 'CELf154sdfgSDFGg');
 bitcoin.createNewBlock(285499, '324dsFQx4ws', 'fdh765DSQAxn');
 
 console.log(bitcoin);
+*/
